1d54c20382ced6dd20a5aabd20d3499b2c8080a1
gyojincompany <luckyred@gmail.com>
2024-05-27 16:14:49 +0900

    vector 예제 추가

---
+++ src/com/gyojincompany/exer02/VectorExample.java
@@ -0,0 +1,22 @@
+package com.gyojincompany.exer02;
+
+import java.util.List;
+import java.util.Vector;
+
+public class VectorExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		List<Board> boardList = new Vector<Board>();
+		
+		boardList.add(new Board("가입인사드려요", "반갑습니다. 오늘 가입했어요", "홍길동"));
+		boardList.add(new Board("가입인사드려요", "반갑습니다. 오늘 가입했어요", "홍길동"));
+		boardList.add(new Board("가입인사드려요", "반갑습니다. 오늘 가입했어요", "홍길동"));
+		boardList.add(new Board("가입인사드려요", "반갑습니다. 오늘 가입했어요", "홍길동"));
+		boardList.add(new Board("가입인사드려요", "반갑습니다. 오늘 가입했어요", "홍길동"));
+		
+		
+	}
+
+}
